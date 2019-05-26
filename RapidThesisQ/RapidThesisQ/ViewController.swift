5484a8c360ec202a960fcd26720e061cc07d599d
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2019-05-26 09:05:40 +0800

    First Try

---
+++ RapidThesisQ/RapidThesisQ/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RapidThesisQ
+//
+//  Created by Macbook Pro on 07/05/2019.
+//  Copyright © 2019 Usim. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import TextFieldEffects
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
